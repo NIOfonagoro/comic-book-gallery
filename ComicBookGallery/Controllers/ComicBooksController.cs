b2cde146382c194849e7c44ad52b5112635b502a
Nicholas Ofonagoro <nofonagoro@spartaglobal.com>
2018-10-12 11:31:11 +0100

    Moved info to controller. Used dynamix types, although dynamic types (ViewBag) aren't recommended

---
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComicBookGallery.Controllers
+{
+    public class ComicBooksController : Controller
+    {
+        public ActionResult Detail()
+        {
+            //if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
+            //{
+            //    return new RedirectResult("/");
+            //}
+
+            //return Content("Hello from the Comic Book Controller");
+            ViewBag.SeriesTitle = "THe Amazing Spider-Man";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
+            ViewBag.Artists = new string[]
+            {
+                "Script: Dan Slott",
+                "Pencils: Humberto Ramos",
+                "Inks: Victor Olazaba",
+                "Colors: Edgar Delgado",
+                "Letters: Chris Eliopoulos"
+            };
+            return View();
+        }
+    }
+}
